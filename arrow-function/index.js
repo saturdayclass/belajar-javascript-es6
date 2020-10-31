4167f0c384c46d8074b810469ebcc90c24a876b0
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-11-01 05:39:00 +0700

    add: example arrow function in javascript es6

---
+++ arrow-function/index.js
@@ -0,0 +1,14 @@
+// Deklrasi
+const MyFunction = (nama) => {
+  return console.log(nama);
+};
+
+MyFunction('Raihan');
+
+// Expressi
+
+const myArray = [1, 2];
+
+myArray.map((item) => {
+  return console.log(item);
+});
